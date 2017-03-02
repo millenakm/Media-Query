1ca1ca719ecbc60f3722e99de490fceb067e2442
millenakm <millena.martins@digitaldesk.com.br>
2017-03-02 16:15:07 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		var scrollPos = $(window).scrollTop();
+		navbar = $('.navbar');
+
+		if (scrollPos > 50) {
+			navbar.addClass('alt-color');
+		} else {
+			navbar.removeClass('alt-color');
+		}
+	});
+	
+
+	$(document).on("scroll", onScroll);
+
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+		$('a').each(function () {
+			$(this).removeClass('active');
+		});
+		$(this).addClass('active');
+		var target = this.hash,
+		menu = target;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 600, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
+	});
+});
+
+function onScroll(event){
+	var scrollPos = $(document).scrollTop();
+	$('nav a').each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top-66 <= scrollPos && refElement.position().top -66+ refElement.height() >= scrollPos) {
+			$('nav ul li a').removeClass("active");
+			currLink.addClass("active");
+		}
+		else{
+			currLink.removeClass("active");
+		}
+	});
+}
